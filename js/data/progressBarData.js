b7b2f7391122ac9bfb804e4bf181f8c71cf3bc8a
Roberta2020 <74533119+Roberta2020@users.noreply.github.com>
2020-12-16 10:36:12 +0200

    render progress bar

---
+++ js/data/progressBarData.js
@@ -0,0 +1,16 @@
+const progressBarData = [
+    {
+        title: 'UX design',
+        value: 90
+    },
+    {
+        title: 'web design',
+        value: 50
+    },
+    {
+        title: 'web developement',
+        value: 65
+    },
+];
+
+export { progressBarData }
