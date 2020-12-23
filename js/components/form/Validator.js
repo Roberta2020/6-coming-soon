aced75755416e91c987390b3387238bc0f5f59fc
Roberta2020 <74533119+Roberta2020@users.noreply.github.com>
2020-12-23 11:25:19 +0200

    onlyFirstUppercaseLetter

--- js/components/form/Validator.js
+++ js/components/form/Validator.js
@@ -1,16 +1,25 @@
 class Validator {
     static isValidName(name) {
         //turi buti ne tuscias tekstas
+        const notEmptyString = Validator.notEmptyString(name);
+        if(notEmptyString !== true) return notEmptyString;
+ 
         //tik abeceles raides
-        //pirma raide didzioji
-        console.log('validuojame name:', name);
+        const onlyAlphabet = Validator.onlyAlphabet(name);
+        if(onlyAlphabet !== true) return onlyAlphabet;
+
+        //tik pirma raide didzioji
+        const onlyFirstLetterUppercase = Validator.onlyFirstLetterUppercase(name);
+        if(onlyFirstLetterUppercase !== true) return onlyFirstLetterUppercase;
+
         return true;
     }
 
     static isValidEmail(email) {
         //turi buti ne tuscias tekstas
-        if() {
-            
+        const b = Validator.notEmptyString(email);
+        if(b !== true) {
+            return b;
         }
         //ne trumpesnis nei 6 simboliai
         // tik 1 @ simboolis
@@ -24,6 +33,9 @@ class Validator {
     }
     static isValidMessage(message) {
         //turi buti ne tuscias tekstas
+        if(typeof message === '') {
+            return 'Turi buti ne tuscias tekstas.'
+        }
         //nevirsyti 1000 simboliu
         console.log('validuojame message:', message);
         return true;
@@ -38,6 +50,35 @@ class Validator {
         }
         return true;
     }
+
+
+
+    static onlyAlphabet(text) {
+        return true;
+    }
+    static onlyFirstLetterUppercase(text) {
+        // aaBasaa -> aabasaa -> Aabasaa false
+        // aaaaaBa -> aaaaaba -> Aaaaaba false
+        // Baaaaaa -> baaaaaa -> Baaaaaa true
+
+        //1) visas raides konvertuoti i mazasias
+        //2) pirmaja raide paverciame didziaja
+        //3) palyginame ka gavome su input
+
+        //1. 
+        let lower = text.toLowerCase();
+        //2.
+        let convert = lower[0].toUpperCase() + lower.slice(1);
+        //3.
+        if(text !== convert) {
+            return 'Pirmasis simbolis turi buti didzioji raide, o visi kiti mazosios.';
+        }
+
+        return true;
+    }
+
 }
 
 export { Validator }
+
+
